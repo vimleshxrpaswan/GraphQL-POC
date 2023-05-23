24b2e9f2a6c6d05dc7c60c1ac9d451bb0e694562
vimleshxrpaswan <paswanvimlesh481@gmail.com>
2023-05-23 14:56:53 +0530

    getting started with graphQL

--- src/main/java/com/graphql/controller/BookController.java
+++ src/main/java/com/graphql/controller/BookController.java
@@ -10,7 +10,6 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
